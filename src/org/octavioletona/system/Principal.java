cbbe94c3b3ff21d83d4773b6998ef9a6c24d0884
Octavio Letona <oletona-2026097@kinal.edu.gt>
2026-06-12 11:24:53 -0600

    Primer commit, creacion de proyecto, creacion de estructura del proyecto

---
+++ src/org/octavioletona/system/Principal.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package org.octavioletona.system;
+
+/**
+ *
+ * @author informatica
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+        // TODO code application logic here
+    }
+    
+}
